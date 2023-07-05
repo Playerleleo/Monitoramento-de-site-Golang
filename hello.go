2d63acc41daaa9649d9ba9f54759b1859965da6a
Playerleleo <leonardogomesdossantos123@gmail.com>
2023-07-05 11:53:15 -0300

    Primeiro commit

---
+++ hello.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	nome := "douglas"
+	versao := 1.1
+
+	fmt.Println("Olá senhor(a)", nome)
+	fmt.Println("Este programa esta na versao", versao)
+
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
+
+}
